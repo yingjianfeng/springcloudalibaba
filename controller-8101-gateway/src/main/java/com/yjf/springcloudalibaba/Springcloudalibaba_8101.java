361514382c3e146718a116a7414f442bf432cbc1
yjf <1374993958@qq.com>
2020-10-16 18:09:30 +0800

    日常代码提交

--- controller-8101-gateway/src/main/java/com/yjf/springcloudalibaba/Springcloudalibaba_8101.java
+++ controller-8101-gateway/src/main/java/com/yjf/springcloudalibaba/Springcloudalibaba_8101.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Springcloudalibaba_8101 {
@@ -19,6 +20,9 @@ public class Springcloudalibaba_8101 {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/nihao")
+                        .filters(
+                                f -> f.stripPrefix(1)
+                        )
                         .uri("http://www.yjf.com:8102/get/getMapping")   //不知道为什么 http://www.baidu.com 不能跳
                 ).build();
     }
